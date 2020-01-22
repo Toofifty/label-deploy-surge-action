005fb6fd42008e10ae83f239da8899d39ed00bb5
Toofifty <alex@matho.me>
2020-01-23 10:45:40 +1100

    testing

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
+const surge = require('surge')({ default: 'publish' });
+
+const slug = branch => branch.replace(/[.]/, '-');
 
 // most @actions toolkit packages have async methods
-async function run() {
+const run = async () => {
     try {
-        const ms = core.getInput('milliseconds');
-        console.log(`Waiting ${ms} milliseconds ...`);
+        const dist = core.getInput('dist-folder');
+        process.env.SURGE_TOKEN = core.getInput('surge-token');
+        process.env.SURGE_LOGIN = core.getInput('surge-login');
+        const url = 'test-domain-sadfkjhio.surge.sh';
 
-        core.debug(new Date().toTimeString());
+        console.log(`Deploying to surge.sh from ${dist}`);
+        console.log(github.context);
+
+        surge([dist, url]);
 
         core.setOutput('time', new Date().toTimeString());
+        core.setOutput('surge-url', url);
     } catch (error) {
         core.setFailed(error.message);
     }
-}
+};
 
 run();

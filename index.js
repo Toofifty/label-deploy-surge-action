e37b18f010621e447b28596ee423043f047614cf
Toofifty <alex@matho.me>
2020-01-23 11:35:18 +1100

    things

--- index.js
+++ index.js
@@ -2,23 +2,51 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const surge = require('surge')({ default: 'publish' });
 
-const slug = branch => branch.replace(/[.]/, '-');
+const TASK_REGEX = /^\[([\w-\d]+)\]/;
 
-// most @actions toolkit packages have async methods
+const slug = branch => branch.toLowerCase().replace(/[.]/, '-');
+
+const makeDomain = pullRequest => {
+    const { sha } = github.context;
+    const { title } = pullRequest;
+
+    const titleMatch = title.match(TASK_REGEX);
+
+    if (titleMatch) {
+        return slug(titleMatch[1]);
+    }
+
+    return sha.substring(0, 8);
+};
+
+const shouldDeploy = pullRequest => {
+    const label = core.getInput('label');
+
+    return (
+        pullRequest && pullRequest.labels && pullRequest.labels.includes(label)
+    );
+};
+
 const run = async () => {
     try {
+        const { pull_request: pullRequest } = github.context.payload;
+        if (!shouldDeploy(pullRequest)) {
+            console.log('Label not found, skipping');
+            return;
+        }
+
+        const domain = makeDomain(pullRequest);
         const dist = core.getInput('dist-folder');
         process.env.SURGE_TOKEN = core.getInput('surge-token');
         process.env.SURGE_LOGIN = core.getInput('surge-login');
-        const url = 'test-domain-sadfkjhio.surge.sh';
 
         console.log(`Deploying to surge.sh from ${dist}`);
         console.log(github.context);
 
-        surge([dist, url]);
+        surge([dist, domain]);
 
         core.setOutput('time', new Date().toTimeString());
-        core.setOutput('surge-url', url);
+        core.setOutput('surge-url', domain);
     } catch (error) {
         core.setFailed(error.message);
     }
